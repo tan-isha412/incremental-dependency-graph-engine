4590f8de481de269cde559ebc44387998c1c8f96
tan-isha412 <tanishasaha412@gmail.com>
2026-09-05 16:48:28 +0530

    dependency parser fixed

--- backend/app/src/main/java/org/example/scanner/JavaDependencyParser.java
+++ backend/app/src/main/java/org/example/scanner/JavaDependencyParser.java
@@ -1,12 +1,12 @@
 package org.example.scanner;
 
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.List;
 
 public class JavaDependencyParser {
-    
+
     public List<String> getImports(Path javaFile) {
-
-
-}
+        return Collections.emptyList();
+    }
 }
